a5989b254bf864dc936b79ec6eee0082b867ea56
ido555 <13142010+ido555@users.noreply.github.com>
2021-02-27 13:09:03 +0200

    a

--- App/App/wsgi.py
+++ App/App/wsgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'App.settings')
 
 application = get_wsgi_application()
+
+
+# TODO add note about this in report
+call_command("flush", "--noinput")
+call_command("makemigrations", "--noinput")
+call_command("migrate", "--noinput")
+print("finished resetting DB")
--- App/Dcoya/views.py
+++ App/Dcoya/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, QueryDict
+from http import HTTPStatus
 
 
 def index(request):
-    return HttpResponse('<h2 style="text-align: center">Hey there</h2>')
+    return HttpResponse('<h2 style="text-align: center">Index</h2>')
+
+
+def registerClient(Request: WSGIRequest):
+    if Request.POST is not None:
+        data: QueryDict = Request.POST
+        if 'username' in data.keys() and 'password' in data.keys():
+            return HttpResponse('Mock Token')
+
+    return HttpResponse(status=HTTPStatus.EXPECTATION_FAILED,
+                        content="must include 'username' and 'password' parameters in a POST request")
