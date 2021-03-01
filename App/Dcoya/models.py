971891f2115d129770d51349d7692c88af060b8b
ido555 <13142010+ido555@users.noreply.github.com>
2021-03-01 12:36:08 +0200

    a

--- App/Dcoya/models.py
+++ App/Dcoya/models.py
@@ -12,6 +12,7 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=255, default="")
-    password = models.CharField(max_length=255, default="")
+    username = models.CharField(max_length=256, default="")
+    password = models.CharField(max_length=256, default="")
+    # token length wont exceed 820 characters given a unix timestamp + 512 characters in SHA256
     token = models.CharField(max_length=1024, default="")
--- App/Dcoya/views.py
+++ App/Dcoya/views.py
@@ -6,33 +6,33 @@ from .models import User
 from django.db.models.manager import Manager, QuerySet
 from django.db.utils import Error
 from jwt import encode, decode, PyJWTError
-from datetime import datetime, time
+from time import time
 
 secretiveSecret = "fpj2u6fCLsHeb6TjuvFBC9ULNmpN4xC9qW6Cz57wU969jzpEA2mPaCvb2rmwpXZXpTVWEShM4S75ysjeB3wBU3Y9QSQQu2SvwQqj5jWRMH5FWQCDCgAQuFWTdwPSKVXN"
+timeToExpiration = 3600  # seconds
 
 
 def registerClient(request: WSGIRequest):
     print("running registerClient()")
     if request.method == "POST":
         data: dict = jsonLoads(request.body)
-        if len(data.keys()) != 2:
+        if len(data.keys()) != 2 or "username" and "password" not in data.keys():
             return HttpResponse(status=HTTPStatus.EXPECTATION_FAILED,
                                 content="must include only 2 parameters: 'username' and 'password' in a JSON format")
 
         for k, v in data.items():
             if k == "username" or "password":
-                if len(v) > 255:
+                if len(v) > 256:
                     return HttpResponse(status=HTTPStatus.REQUEST_ENTITY_TOO_LARGE,
-                                        content="each JSON parameter should not exceed 255 characters in size")
+                                        content="each JSON parameter should not exceed 256 characters in size")
         try:
             if isUsernameExists(data.get('username')):
                 return HttpResponse(status=HTTPStatus.CONFLICT, content="This username is taken")
             token = insertUser(data.get('username'), data.get('password'))
             return HttpResponse(status=HTTPStatus.OK, content=token)
         except Error:
-            return HttpResponse(HTTPStatus.INTERNAL_SERVER_ERROR, 'Something went  wrong, please try again later')
-
-        return HttpResponse('Mock Token')
+            return HttpResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR,
+                                content='Something went  wrong, please try again later')
     return HttpResponse(status=HTTPStatus.EXPECTATION_FAILED,
                         content="Must include 'username' and 'password' in a POST request's body in a JSON format")
 
@@ -54,7 +54,7 @@ def isUsernameExists(username: str):
         return QuerySet(model=User).all().filter(username=username).exists()
     except Error as e:
         print("Error at isUsernameExists()")
-        raise str(e)
+        raise e
 
 
 def isUsernameAndPasswordExists(username: str, password: str):
@@ -63,7 +63,7 @@ def isUsernameAndPasswordExists(username: str, password: str):
         return QuerySet(model=User).all().filter(username=username, password=password).exists()
     except Error as e:
         print("Error at isUsernameAndPasswordExists()")
-        raise str(e)
+        raise e
 
 
 def getAllUsers():
@@ -75,36 +75,39 @@ def getAllUsers():
         return QuerySet(model=User).all()
     except Error as e:
         print("Error at getAllUsers()")
-        raise str(e)
+        raise e
 
 
 def insertUser(username: str, password: str):
     print("running insertUser()")
     token = ""
+    expirationDate = int(time()) + timeToExpiration
     try:
         user: User = User()
         user.username = username
         user.password = password
         # TODO add expiry date
 
-        token = str(encode({"usr": username, "pswd": password}, secretiveSecret, "HS256"))
+        token = str(encode({"usr": username, "pswd": password, "expDate": expirationDate}, secretiveSecret, "HS256"))
         user.token = token
         user.save()
         print(f"{username}'s token is {token}")
         return token
     except Error as e:
         print("Error at insertUser()")
-        raise str(e)
+        raise e
     except PyJWTError as e:
         print("PyJWTError at insertUser()")
-        raise str(e)
+        raise e
 
 
 def authorizeUser(jwtToken: str):
     print("running authorizeUser()")
     try:
         payload = decode(jwtToken, secretiveSecret, "HS256")
-        if "usr" and "pswd" in payload.keys():
+        if "usr" and "pswd" and "expDate" in payload.keys():
+            if payload.get("expDate") > int(time()):
+                return False
             if isUsernameAndPasswordExists(payload.get("usr"), payload.get("pswd")):
                 return True
         return False
@@ -113,8 +116,22 @@ def authorizeUser(jwtToken: str):
         return False
 
 
-def echo(encodedJwt: str):
+def echo(request: WSGIRequest):
+    # encodedJwt: str, msg: str
+    if request.method == "POST":
+        data: dict = jsonLoads(request.body)
+        if len(data.keys()) != 2 or "encodedJwt" and "msg" not in data.keys():
+            return HttpResponse(status=HTTPStatus.EXPECTATION_FAILED,
+                                content="must include only 2 parameters: 'msg' and 'encodedJwt' in a JSON format")
+        try:
+
+            return HttpResponse(status=HTTPStatus.OK, content="")
+        except Error:
+            return HttpResponse(status=HTTPStatus.INTERNAL_SERVER_ERROR,
+                                content='Something went  wrong, please try again later')
     pass
 
 
-print(time.time())
+for user in getAllUsers():
+    print(user.username)
+    print(decode(user.token, secretiveSecret, "HS256"))
