f35e212086f2789937a7dd215fe2ce0146b81196
ido555 <13142010+ido555@users.noreply.github.com>
2021-03-02 18:09:04 +0200

    a

--- App/Dcoya/tests.py
+++ App/Dcoya/tests.py
@@ -1,13 +1,13 @@
-from django.db.models import QuerySet
-from django.test import TestCase
-from .models import User
-from django.core.exceptions import ValidationError
-
-
-class UserModelTests(TestCase):
-    def test_usernameTooLong(self):
-        user = User(username="a"*400)
-        # print(user.username)
-        # user.save()
-        # dbUsername = QuerySet(model=User).last().username
-        self.assertRaises(ValidationError, user.full_clean(), True)
+# from django.db.models import QuerySet
+# from django.test import TestCase
+# from .models import User
+# from django.core.exceptions import ValidationError
+# initial attempt at automated tests - ignore
+#
+# class UserModelTests(TestCase):
+#     def test_usernameTooLong(self):
+#         user = User(username="a"*400)
+#         # print(user.username)
+#         # user.save()
+#         # dbUsername = QuerySet(model=User).last().username
+#         self.assertRaises(ValidationError, user.full_clean(), True)
